7eb16812459b1a5d448293abb440c94d08d15510
halexand <harriet.xander@gmail.com>
2018-12-06 15:04:29 -0500

    added parsable flag bug fix

--- gen-profile/submit-slurm.sh
+++ gen-profile/submit-slurm.sh
@@ -2,6 +2,6 @@
 snakemake   \
     --jobs 200           \
         --use-conda                         \
-            --profile gen-slurm-2		\
-		--cluster "sbatch --partition=compute --job-name=TARA.{rule}.{wildcards} --mem=1gb --time=24:00:00 --ntasks=1 --nodes=1"
+            --profile general-slurm-profile		\
+		--cluster "sbatch --parsable  --partition=compute --job-name=TARA.{rule}.{wildcards} --mem=1gb --time=24:00:00 --ntasks=1 --nodes=1"
 
